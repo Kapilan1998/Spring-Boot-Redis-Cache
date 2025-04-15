d575a977710ff52fc286884ae1bb26269b01c747
Kapilan <sriranjankapilan@gmail.com>
2025-04-16 00:38:13 +0530

    entity added

---
+++ src/main/java/com/spring/cache/redis/entity/Product.java
@@ -0,0 +1,21 @@
+package com.spring.cache.redis.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
