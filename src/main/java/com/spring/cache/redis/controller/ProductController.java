23bd47029aab6890083cd7ad67358ea6b94a585b
Kapilan <sriranjankapilan@gmail.com>
2025-04-16 00:39:13 +0530

    controller added

---
+++ src/main/java/com/spring/cache/redis/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.spring.cache.redis.controller;
+
+import com.spring.cache.redis.dto.ProductDto;
+import com.spring.cache.redis.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ProductDto createProduct(@RequestBody @Valid ProductDto productDto) {
+        return productService.createProduct(productDto);
+    }
+
+    @GetMapping("/{productId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public ProductDto getProduct(@PathVariable Long productId) {
+        return productService.getProduct(productId);
+    }
+
+    @PutMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public ProductDto updateProduct(@RequestBody @Valid ProductDto productDto) {
+        return productService.updateProduct(productDto);
+    }
+
+    @DeleteMapping("/{productId}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
+    }
+}
