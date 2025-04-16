1d5823f5cf14d506701f62392bb76890d398a37c
Kapilan <sriranjankapilan@gmail.com>
2025-04-16 13:23:53 +0530

    cache related annotations added

--- src/main/java/com/spring/cache/redis/service/ProductService.java
+++ src/main/java/com/spring/cache/redis/service/ProductService.java
@@ -3,6 +3,9 @@ package com.spring.cache.redis.service;
 import com.spring.cache.redis.dto.ProductDto;
 import com.spring.cache.redis.entity.Product;
 import com.spring.cache.redis.repository.ProductRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,13 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * Forces a cache update after the method is executed.
+     * It stores the return value (ProductDto) in the cache.
+     * The cache key is result.id() — meaning the product ID returned from the method
+    * When we create a new product, we want to add it to the cache immediately so that subsequent getProduct() calls can return it from cache.
+     */
+    @CachePut(value = "PRODUCT_CACHE", key = "#result.id()")
     public ProductDto createProduct(ProductDto productDto) {
         var product = new Product();
         product.setName(productDto.name());
@@ -23,6 +33,13 @@ public class ProductService {
                 savedProduct.getPrice());
     }
 
+    /**
+     * Checks the cache first using productId as the key.
+     * If the key is found in cache: returns the cached value, skips executing the method.
+     * If the key is not found: runs the method, caches the result using productId as the key, then returns it.
+     * When reading data (like a product), this avoids unnecessary DB calls by using cached results whenever possible.
+     */
+    @Cacheable(value ="PRODUCT_CACHE", key = "#productId")
     public ProductDto getProduct(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("Cannot find product with id " + productId));
@@ -30,6 +47,11 @@ public class ProductService {
                 product.getPrice());
     }
 
+    /**
+     * It uses the returned product ID (#result.id()) as the cache key.
+     *After updating a product, you want the cache to reflect the latest data — so @CachePut ensures the cache is refreshed even if the method is always executed.
+     */
+    @CachePut(value = "PRODUCT_CACHE", key = "#result.id()")
     public ProductDto updateProduct(ProductDto productDto) {
         Long productId = productDto.id();
         Product product = productRepository.findById(productId)
@@ -43,6 +65,12 @@ public class ProductService {
                 updatedProduct.getPrice());
     }
 
+    /**
+     *Removes the entry from the cache for the given productId.
+     * Makes sure the deleted product is no longer returned from the cache
+     * Once a product is deleted from the DB, we don't want stale data in the cache — @CacheEvict ensures that.
+     */
+    @CacheEvict(value = "PRODUCT_CACHE", key = "#productId")
     public void deleteProduct(Long productId) {
         productRepository.deleteById(productId);
     }
