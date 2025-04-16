70f5bf27b439cd4647fe8648e4b19c962fdd2bf0
Kapilan <sriranjankapilan@gmail.com>
2025-04-16 13:23:35 +0530

    redis enabled

--- src/main/java/com/spring/cache/redis/SpringBootRedisCacheApplication.java
+++ src/main/java/com/spring/cache/redis/SpringBootRedisCacheApplication.java
@@ -2,8 +2,10 @@ package com.spring.cache.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching      // caching related functionalities are enabled
 public class SpringBootRedisCacheApplication {
 
     public static void main(String[] args) {
